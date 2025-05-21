4648f2068567d187c47ba98d9361270035b42676
Kyurtem <kyuremasturiat@gmail.com>
2025-05-21 14:02:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title('🍷🍷🍷 Machine learning Wine stadistic 🍷🍷🍷')
-st.write('Esta es un app que nos permite ver las estadistica de los catadores de vino')
+st.info('Esta es un app que nos permite ver las estadistica de los catadores de vino')
