92568adad4d8fa5b8eefd2de1d384f7a27cde03f
Kyurtem <kyuremasturiat@gmail.com>
2025-05-21 14:19:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 st.title('🍷🍷🍷 Machine learning Wine stadistic 🍷🍷🍷')
 st.info('Esta es un app que nos permite ver las estadistica de los catadores de vino')
+
+df = pd.read_csv('https://github.com/Kyurtem/dp-machinelearning/blob/main/winequality-red.csv')
+df 
